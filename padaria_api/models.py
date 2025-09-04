d75bac9d4201bacc76816180a13a6577753b4ce2
Robson <rbn.alves@hotmail.com>
2025-09-04 16:55:14 -0300

    upd

--- padaria_api/models.py
+++ padaria_api/models.py
@@ -3,42 +3,56 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-# Modelo para o produto
-class Produto(db.Model):
-    id = db.Column(db.String(50), primary_key=True) # Código de barras ou identificação
+class ProdutoEstoque(db.Model):
+    __tablename__ = 'produtos_estoque'
+    id = db.Column(db.String(50), primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     valor = db.Column(db.Float, nullable=False)
-    quantidade = db.Column(db.Integer, nullable=False, default=0)
-    
-    def __repr__(self):
-        return f'<Produto {self.nome}>'
+    quantidade = db.Column(db.Integer, default=0)
 
-# Modelo para a venda
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'valor': self.valor,
+            'quantidade': self.quantidade
+        }
+
 class Venda(db.Model):
+    __tablename__ = 'vendas'
     id = db.Column(db.Integer, primary_key=True)
-    data_hora = db.Column(db.DateTime, default=datetime.utcnow)
-    tipo_venda = db.Column(db.String(20), nullable=False) # 'Balcao' ou 'Delivery'
-    plataforma = db.Column(db.String(50), nullable=True) # Ex: 'Ifood', '99food'
-    forma_pagamento = db.Column(db.String(20), nullable=False) # 'Debito', 'Credito', 'Pix', 'Dinheiro'
     valor_total = db.Column(db.Float, nullable=False)
-    cancelada = db.Column(db.Boolean, default=False)
-    
-    # Relação com ItemVenda
-    itens = db.relationship('ItemVenda', backref='venda', lazy=True)
-    
-    def __repr__(self):
-        return f'<Venda {self.id}>'
+    forma_pagamento = db.Column(db.String(50), nullable=False)
+    valor_recebido = db.Column(db.Float, nullable=True)
+    data_hora = db.Column(db.DateTime, default=datetime.now)
+    tipo_venda = db.Column(db.String(50), nullable=False) # 'balcao' ou 'delivery'
 
-# Modelo para os itens de uma venda
-class ItemVenda(db.Model):
+    # Relacionamento com a tabela VendaItem
+    itens_vendidos = db.relationship('VendaItem', backref='venda', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'valor_total': self.valor_total,
+            'forma_pagamento': self.forma_pagamento,
+            'data_hora': self.data_hora.isoformat(),
+            'tipo_venda': self.tipo_venda
+        }
+
+class VendaItem(db.Model):
+    __tablename__ = 'venda_itens'
     id = db.Column(db.Integer, primary_key=True)
-    venda_id = db.Column(db.Integer, db.ForeignKey('venda.id'), nullable=False)
-    produto_id = db.Column(db.String(50), db.ForeignKey('produto.id'), nullable=False)
+    venda_id = db.Column(db.Integer, db.ForeignKey('vendas.id'), nullable=False)
+    produto_id = db.Column(db.String(50), db.ForeignKey('produtos_estoque.id'), nullable=False)
     quantidade = db.Column(db.Integer, nullable=False)
-    valor_unitario = db.Column(db.Float, nullable=False)
-    
-    # Relação com Produto
-    produto = db.relationship('Produto', backref='itens_venda', lazy=True)
-    
-    def __repr__(self):
-        return f'<ItemVenda {self.quantidade}x {self.produto.nome}>'
+
+    # Relacionamento com a tabela ProdutoEstoque
+    produto = db.relationship('ProdutoEstoque', backref=db.backref('venda_itens', lazy=True))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'venda_id': self.venda_id,
+            'produto_id': self.produto_id,
+            'quantidade': self.quantidade
+        }
