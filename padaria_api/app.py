2b38ce03b663a092c4d4eb19e84c5c009c64043d
Robson <rbn.alves@hotmail.com>
2025-09-05 13:43:51 -0300

    at

--- padaria_api/app.py
+++ padaria_api/app.py
@@ -1,9 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from config import Config
 from extensions import db
-
-# Importe os blueprints (rotas)
 from routes.vendas_bp import vendas_bp
 from routes.estoque_bp import estoque_bp
 from routes.relatorios_bp import relatorios_bp
@@ -11,20 +9,17 @@ from routes.relatorios_bp import relatorios_bp
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Inicializa o banco de dados com o app
 db.init_app(app)
 
-# Registra os blueprints com um prefixo de URL
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 app.register_blueprint(vendas_bp, url_prefix='/api')
 app.register_blueprint(estoque_bp, url_prefix='/api')
 app.register_blueprint(relatorios_bp, url_prefix='/api')
 
-# Configura o CORS para permitir requisições do frontend
-CORS(app, resources={r"/api/*": {"origins": "*"}})
-
 @app.route('/')
-def home():
-    return 'API da Padaria Majurak está no ar!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     with app.app_context():
